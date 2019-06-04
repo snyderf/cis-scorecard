404bca0b1a75e04e91e6cff2494bf49f9e4c2ed3
nvpnathan <nathan79@gmail.com>
2019-06-03 22:04:48 -0700

    changed log output

--- kubelet_score.sh
+++ kubelet_score.sh
@@ -4,6 +4,6 @@ set -o nounset
 RESULTS_DIR="${RESULTS_DIR:-/tmp/results}"
 
 ps -ef |grep kubelet >"${RESULTS_DIR}/kubelet_config"
-echo -n "${RESULTS_DIR}/kubelet_logs" >"${RESULTS_DIR}/done"
+echo -n "${RESULTS_DIR}/kubelet_config" >"${RESULTS_DIR}/done"
 
 
